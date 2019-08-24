cff17335c71c02653a7d337c0279ebeb787a4db5
zoom <zoom@Zoom>
2019-08-25 01:46:23 +0200

    initialisation du projet

---
+++ exam/src/exam/exoDecembre.java
@@ -0,0 +1,12 @@
+package exam;
+import java.util.Scanner;
+
+public class exoDecembre {
+
+	public static void main(String[] args)
+	{
+		Scanner reader = new Scanner(System.in);
+		int pays[] = new int[24];
+		
+	}
+}
